9871c543c03c143784f993d4c73b956f5d3b5c54
cl-victor1 <cheng_long_@126.com>
2024-07-12 15:47:18 +0800

    lab2

--- fa20-lab-starter/lab02/lfsr.c
+++ fa20-lab-starter/lab02/lfsr.c
@@ -6,5 +6,7 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t single_bit = ((*reg >> 0) & 1) ^ ((*reg >> 2) & 1) ^ ((*reg >> 3) & 1) ^ ((*reg >> 5) & 1);
+    *reg = (*reg >> 1) | (single_bit << 15);
 }
 
